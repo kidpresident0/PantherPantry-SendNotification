3e9f97aaf5ed43ea3361700e0087aff1847dd1eb
John C <john.christian1@pcc.edu>
2023-01-26 13:34:15 -0800

    First commit to Null repo. Started on some very basic SendNotification functinality.

---
+++ src/sendNotification/Main.java
@@ -0,0 +1,10 @@
+package sendNotification;
+
+/**
+ * This is the main class for the Panther Pantry Create sendNotification.Notification application
+ * @author John Christian
+ * @version 2022.01.26
+ */
+public class Main {
+
+}
---
+++ src/sendNotification/NotificationDB.java
@@ -0,0 +1,33 @@
+package sendNotification;
+
+import sendNotification.Notification;
+
+/**
+ * This is the sendNotification.NotificationDB class that will retrieve information needed to send notifications like
+ * subscribers from the dtabase. Eventually it will store some of that information in the database
+ * for the log search feature.
+ * @author John Christian
+ * @version 2022.01.26
+ */
+import java.util.ArrayList;
+
+public class NotificationDB {
+
+    private static final String url = "jdbc:mysql://cisdbss.pcc.edu:3306/234a_Null";
+    private static final String username = "234a_Null";
+    private static final String password = "456_$%^234a_Null";
+    private static final String dbname = "234a_Null";
+
+    private static final String GET_SUBSCRIBERS_SQL = "SELECT * FROM subscribers where ? = ";
+
+
+
+
+    private ArrayList<Notification>readSubscribers() {
+        ArrayList<Notification> subscribers = new ArrayList<>();
+
+        return subscribers;
+
+    }
+}
+
