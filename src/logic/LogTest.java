6d1baba14d2bd19cacf45c90ce2632f4438d9f13
Brandon King <brandon.king4@pcc.edu>
2023-02-28 14:02:38 -0800

    created initial test class in logic.log

---
+++ src/logic/LogTest.java
@@ -0,0 +1,31 @@
+package logic;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogTest {
+
+    @Test
+    void findLogs() {
+    }
+
+    @Test
+    void getSentBy() {
+    }
+
+    @Test
+    void getSentDateTime() {
+    }
+
+    @Test
+    void getSubject() {
+    }
+
+    @Test
+    void getMessageBody() {
+    }
+
+    @Test
+    void getSubscriberCount() {
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import presentation.*;
+
 import javax.swing.*;
 
 
