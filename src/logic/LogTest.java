99fb0b84caf063cac8b1a0c503eb25157530790d
Brandon King <brandon.king4@pcc.edu>
2023-03-01 12:15:11 -0800

    completed test class. everything passes

--- src/logic/LogTest.java
+++ src/logic/LogTest.java
@@ -1,31 +1,46 @@
 package logic;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LogTest {
 
     @Test
-    void findLogs() {
+    void testFindLogs() {
+        ArrayList<Log> logs = Log.findLogs("", "");
+        assertEquals(0, logs.size());
     }
 
     @Test
-    void getSentBy() {
+    void testGetSentBy() {
+        Log l = new Log("Test User","2022-03-01 11:30:00","Test Subject","This is a test message body.",10);
+        assertEquals("Test User", l.getSentBy());
     }
 
     @Test
-    void getSentDateTime() {
+    void testGetSentDateTime() {
+        Log l = new Log("Test User","2022-03-01 11:30:00","Test Subject","This is a test message body.",10);
+        assertEquals("2022-03-01 11:30:00", l.getSentDateTime());
     }
 
     @Test
-    void getSubject() {
+    void testGetSubject() {
+        Log l = new Log("Test User","2022-03-01 11:30:00","Test Subject","This is a test message body.",10);
+        assertEquals("Test Subject", l.getSubject());
     }
 
     @Test
-    void getMessageBody() {
+    void testGetMessageBody() {
+        Log l = new Log("Test User","2022-03-01 11:30:00","Test Subject","This is a test message body.",10);
+        assertEquals("This is a test message body.", l.getMessageBody());
     }
 
     @Test
-    void getSubscriberCount() {
+    void testGetSubscriberCount() {
+        Log l = new Log("Test User","2022-03-01 11:30:00","Test Subject","This is a test message body.",10);
+        assertEquals(Integer.valueOf(10), l.getSubscriberCount());
     }
 }
